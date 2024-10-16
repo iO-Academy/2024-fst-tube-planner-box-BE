3b77b33aa3240843a8599f8d7457592751e99642
Emma Ferrari <e.ferrari@live.co.uk>
2024-10-16 16:52:07 +0100

    end of Wednesday

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 const mysql = require('promise-mysql')
+const {set} = require("express/lib/application");
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
@@ -14,17 +15,48 @@ const getTubes = () => async (request, response) => {
 const getRoute = () => async (request, response) => {
     const from = request.query.from
     const to = request.query.to
+
+    const fromCode = from.slice(-3)
+    const toCode = to.slice(-3)
+    console.log(fromCode)
     const db = await connection
 
-    const route = await db.query(
-        'SELECT `name`, `code`, `line` FROM `tube_info` WHERE `name` = ? OR `name` = ?',
-        [from, to]
-    )
+    const findingLinesQuery = await db.query('SELECT `line` FROM tube_info WHERE `code` = ? OR `code` = ?',
+        [fromCode, toCode])
 
-    response.json({
-        from: from,
-        to: to
+    const newArray = []
+    const duplicates = []
+    const lineArray = []
+
+   findingLinesQuery.forEach((item) => {
+        lineArray.push(item.line)
     })
+
+    lineArray.forEach((item) => {
+        if(newArray.includes(item)) {
+            duplicates.push(item)
+        }
+        else {
+            newArray.push(item)
+        }
+    })
+
+    // console.log(newArray)
+    console.log(duplicates)
+
+    // for now we are going to use duplicate[0] because we are tired
+
+
+
+    const route = await db.query(
+        'SELECT * FROM tube_info WHERE `name` BETWEEN ? AND ? AND `line` = ?',
+        [toCode, fromCode, duplicates[0]]
+    )
+    console.log(to, from)
+    console.log(route)
+
+
+    response.json(route)
 }
 
 module.exports = {getTubes, getRoute}
